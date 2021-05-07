48f01667c97ff374067502a52b0fe93d65dec331
Davide Tello <tello.davide@gmail.com>
2021-05-07 16:22:04 +0200

    Add standard config

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="flats">
+
+            </div>
+            <div className="map">
+                
+            </div>
+        );
+    }
+}
+
+export default App;
